804118e197173dbc17dbde9f7e30631baa41d47c
EmileBen <73851922+EmileBen@users.noreply.github.com>
2022-04-26 12:47:43 -0700

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,4 +1,3 @@
 SELECT users.name, messages.message FROM users JOIN messages ON users.id = messages.creator_id;
-SELECT users.name, messages.message FROM users JOIN messages ON users.id = messages.creator_id JOIN premium ON premium.users_id = users.id WHERE premium.id = 1;
-SELECT messages.message FROM messages JOIN threads ON messages.thread_id = threads.id WHERE threads.theme LIKE 'Gene%';
-SELECT threads.topic FROM threads JOIN users ON users.id = threads.creator_id WHERE users.name = 'Jack Spencer';
+SELECT messages.message FROM messages JOIN threads ON messages.thread_id = threads.id WHERE threads.theme LIKE 'Mov%';
+SELECT threads.topic FROM threads JOIN users ON users.id = threads.creator_id WHERE users.name = 'Emile Benveniste';
